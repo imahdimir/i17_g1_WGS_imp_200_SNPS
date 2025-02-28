76512ea9720c60d19b719c26e4762525bb79911d
Mahdi <imahdimir@gmail.com>
2025-02-27 22:26:42 -0800

    update

---
+++ n190_lm_g1_plus_g2_on_g1_minus_g2.R
@@ -0,0 +1,95 @@
+list.of.packages <- c("data.table", "dplyr", "magrittr", "tidyverse", "plinkFile", "genio", "arrow", "broom", "glue", "sjPlot", "writexl")
+lapply(list.of.packages, library, character.only = TRUE)
+
+
+csf_dir <- "/Users/mmir/Library/CloudStorage/Dropbox/git/250115_CSF_A21_WGS_imp_200_SNPS"
+models_data_dir <- glue("{csf_dir}/med")
+out_dir <- glue("{csf_dir}/out")
+models_sum_dir <- glue("{out_dir}/lm_g1_plus_g2_on_g1_minus_g2_models_summary")
+
+
+fit_and_save_models <- function(data, rsid_value) {
+  
+  # Step 1: Filter the dataframe for the given rsid
+  filtered_data <- data %>% filter(rsid == rsid_value)
+  print(nrow(filtered_data))
+  quality <- filtered_data$quality[1]
+  
+  # Check if the filtered data is not empty
+  if (nrow(filtered_data) == 0) {
+    stop("No data found for the given rsid.")
+  }
+  
+  model <- lm(g1_plus_g2 ~ g1_minus_g2, data = filtered_data)
+  
+  output_fp <- glue("{models_sum_dir}/{quality}_{rsid_value}.txt")
+  
+  sink(output_fp)  # Redirect output to the first file
+  print(summary(model))
+  sink()  # Stop redirecting output
+  
+  return(model)
+}
+
+
+get_models_details <- function(model, rsid_value, coefs_df, model_metrics_df) {
+  # Extract coefficients and statistics
+  co_df <- tidy(model)
+  co_df[["rsid"]] <- rsid_value
+  
+  # Extract model-level statistics
+  metrics_df <- glance(model)
+  metrics_df[["rsid"]] <- rsid_value
+  
+  # Concatenate co_df to coefs_df
+  coefs_df <- rbind(coefs_df, co_df)
+  
+  # Concatenate metrics_df to model_metrics_df
+  model_metrics_df <- rbind(model_metrics_df, metrics_df)
+  
+  # Return both updated dataframes as a list
+  return(list(coefs_df = coefs_df, model_metrics_df = model_metrics_df))
+}
+
+
+# read models data
+df <- read_parquet(glue("{models_data_dir}/model_data_1.parquet"))
+head(df)
+
+# Example usage
+model <- fit_and_save_models(
+  data = df, 
+  rsid_value = "rs362156"
+)
+summary(model)
+
+models_details <- get_models_details(model, "rs362156", data.frame(), data.frame())
+models_details$coefs_df
+models_details$model_metrics_df
+
+
+
+# Get unique rsid values
+unique_rsids <- unique(df$rsid)
+
+combined_coef_dfs <- data.frame()
+combined_model_metrics_df <- data.frame()
+
+
+# Loop through each unique rsid and apply the function
+for (rsid in unique_rsids) {
+  # Apply the function
+  m <- fit_and_save_models(data = df, rsid = rsid)
+  
+  # Print a message to track progress
+  cat("Processed rsid:", rsid, "\n")
+  
+  models_details <- get_models_details(m, rsid, combined_coef_dfs, combined_model_metrics_df)
+  combined_coef_dfs <- models_details$coefs_df
+  combined_model_metrics_df <- models_details$model_metrics_df
+}
+
+
+write_xlsx(combined_model_metrics_df, glue("{out_dir}/lm_g1_plus_g2_on_g1_minus_g2_models_metrics/lm_g1_plus_g2_on_g1_minus_g2_coefficients.xlsx"))
+write_xlsx(combined_coef_dfs, glue("{out_dir}/lm_g1_plus_g2_on_g1_minus_g2_models_coefficients/lm_g1_plus_g2_on_g1_minus_g2_models_metrics.xlsx"))
+
---
+++ n200_lm_g1_plus_g2_on_g1_minus_g2_coefs_hist.R
@@ -0,0 +1,54 @@
+list.of.packages <- c("data.table", "dplyr", "magrittr", "tidyverse", "plinkFile", "genio", "arrow", "broom", "glue", "sjPlot", "writexl", "readxl", "gt", "ggplot2", "patchwork", "cowplot")
+lapply(list.of.packages, library, character.only = TRUE)
+
+
+models_coefs_dir <- "/Users/mmir/Library/CloudStorage/Dropbox/git/250115_CSF_A21_WGS_imp_200_SNPS/out/lm_g1_plus_g2_on_g1_minus_g2_models_coefficients"
+
+df <- read_excel(glue("{models_coefs_dir}/lm_g1_plus_g2_on_g1_minus_g2_coefficients.xlsx"))
+head(df)
+
+
+# Calculate empirical means for each term and quality
+empirical_means <- df %>%
+  group_by(term) %>%
+  summarize(mean_estimate = mean(estimate), .groups = "drop")
+
+empirical_means
+
+# Define custom colors for vertical lines (ensuring clear distinction)
+vline_colors <- c(
+  "Theoretical Mean" = "black"
+)
+
+# Create the combined plot with legend
+combined_plot_minus <- ggplot(df, aes(x = estimate)) +
+  geom_histogram(alpha = 0.4, position = "identity", bins = 30, color = "black", fill = "steelblue") +
+  
+  # Theoretical Mean Lines
+  geom_vline(data = subset(df, term == "g1_minus_g2"),
+             aes(xintercept = 0, color = "Theoretical Mean"), linewidth = 0.8, linetype = "dashed") +
+  
+  scale_color_manual(name = "Vertical Lines", values = vline_colors, guide = guide_legend(override.aes = list(
+    linetype = "solid",   # Solid lines in legend
+    linewidth = 2,        # Thicker for visibility
+    size = .6  # Custom height of short lines
+  ))) +
+  facet_wrap(~ term, scales = "free") +
+  labs(title = "Histogram for g1 + g2 ~ g1 - g2", x = "Estimate", y = "Frequency") +
+  theme_minimal() +
+  theme(
+    legend.position = "right",
+    legend.spacing.y = unit(20, "pt"),
+    legend.title = element_text(size = 12, face = "bold"),  
+    legend.text = element_text(size = 10),
+    legend.box = "vertical"
+  )
+
+
+print(combined_plot_minus)
+
+
+ggsave(glue("{models_coefs_dir}/coefs_histograms.png"), plot = combined_plot_minus)
+
+
+
