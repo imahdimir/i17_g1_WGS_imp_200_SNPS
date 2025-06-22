e9aea5e6e4468eb5f5015a360af81cafeb49a6bd
Mahdi <imahdimir@gmail.com>
2025-06-22 09:07:52 -0700

    Update c_250__replot_for_BGA_present.R

--- c_250__replot_for_BGA_present.R
+++ c_250__replot_for_BGA_present.R
@@ -13,7 +13,7 @@ if (!require("pacman")) install.packages("pacman")
 library("pacman")
 
 # Installs any missing packages and loads them
-pacman::p_load(readr, ggplot2, dplyr, tidyr, glue, readxl)
+pacman::p_load(readr, ggplot2, dplyr, tidyr, glue, readxl, qqplotr)
 
 ## ---------------------------------------------------------------------
 
@@ -77,80 +77,70 @@ ggsave(fp, plot = plot, dpi = 300, width = 8 * scale, height = 6 * scale)
 
 ## plot qq plot for plus models and add confidence intervals to it
 
-# Low quality
-png(glue('{out_dir}/plus_models_low_quality_dosages.png'), width=6, height=5, units='in', res=300)
 
+
+# read in dosages models coefficients
+fp <- file.path(proj_data_dir, 'out', 'models_coefficients_dsg', 'models_coefficients_dsg.xlsx')
+dos <- readxl::read_xlsx(fp, sheet = 1)
 dos_low = dos[dos$quality=='low' & dos$term=='g1_minus_g2_imp_dsg' & dos$model_name=='plus',]
+
+
+
+gg <- ggplot(data = dos_low, mapping = aes(sample = statistic)) +
+  stat_qq_band() +
+  stat_qq_line() +
+  stat_qq_point() +
+  labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
+gg
+
+fp <- file.path(proj_data_dir, 'out', 'qq_plot_plus_model_low_quality_dosages.png')
+ggsave(fp, plot=gg, width = 6, height = 5, units = 'in', dpi = 300)
+
 qqnorm(dos_low$statistic)
 abline(a=0,b=1,col=2)
 
-dev.off()
 
-
-
-##
-
-
-
-
-out_dir <- '~/Dropbox/git/250115_CSF_A21_WGS_imp_200_SNPS/out/qq_plots'
-
-d = read_xlsx('models_coefficients (1).xlsx',sheet=1)
-# plus model
-d_low = d[d$quality=='low' & d$term=='g1_minus_g2_imp' & d$model_name=='plus',]
-d_high = d[d$quality=='high' & d$term=='g1_minus_g2_imp' & d$model_name=='plus',]
-
-d_high$q = p.adjust(d_high$p.value,method='BH')
-
-d_high[d_high$q<0.1,]
-
-plot(d_high$info_score,d_high$q)
-
-# minus model
-hist(as.matrix(d[d$quality=='low' & d$term=='g1_minus_g2_imp' & d$model_name=='minus','estimate']))
-
-
-### Dosages
-dos = read_xlsx('models_coefficients_dsg.xlsx',sheet=1)
-
-
-# High quality
-png(glue('{out_dir}/plus_models_high_quality_dosages.png'), width=6, height=5, units='in', res=300)
 
 dos_high = dos[dos$quality=='high' & dos$term=='g1_minus_g2_imp_dsg' & dos$model_name=='plus',]
 qqnorm(dos_high$statistic)
 abline(a=0,b=1,col=2)
 
-dev.off()
+gg <- ggplot(data = dos_high, mapping = aes(sample = statistic)) +
+  stat_qq_band() +
+  stat_qq_line() +
+  stat_qq_point() +
+  labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
+gg
+
+fp <- file.path(proj_data_dir, 'out', 'qq_plot_plus_model_high_quality_dosages.png')
+ggsave(fp, plot=gg, width = 6, height = 5, units = 'in', dpi = 300)
 
 
-# Low quality
-png(glue('{out_dir}/plus_models_low_quality_dosages.png'), width=6, height=5, units='in', res=300)
+## ---------------------------------------------------------------------
 
-dos_low = dos[dos$quality=='low' & dos$term=='g1_minus_g2_imp_dsg' & dos$model_name=='plus',]
-qqnorm(dos_low$statistic)
-abline(a=0,b=1,col=2)
+# QQ plot for sibsum sibdiff regression using only WGS data
 
-dev.off()
+fp <- file.path(proj_data_dir, 'med', 'wgs_g1_plus_g2_on_g1_minus_g2_coefficients.xlsx')
+wgs <- readxl::read_xlsx(fp, sheet = 1)
+
+wgs_outlier = wgs[wgs$rsid=='rs11415427',]
+wgs = wgs[wgs$rsid!='rs11415427',]
+wgs = wgs[wgs$term=='g1_minus_g2',]
 
 
-## Minus model
-
-# High quality
-png(glue('{out_dir}/minus_models_high_quality_dosages.png'), width=6, height=5, units='in', res=300)
-
-dos_high_minus = dos[dos$quality=='high' & dos$term=='g1_minus_g2_imp_dsg' & dos$model_name=='minus',]
-dos_high_minus
-qqnorm(dos_high_minus$statistic)
-abline(a=0,b=1,col=2)
-
-dev.off()
-
-# Low quality
-png(glue('{out_dir}/minus_models_low_quality_dosages.png'), width=6, height=5, units='in', res=300)
-
-dos_low_minus = dos[dos$quality=='low' & dos$term=='g1_minus_g2_imp_dsg' & dos$model_name=='minus',]
-qqnorm(dos_low_minus$statistic)
+qqnorm(wgs$statistic)
 abline(a=0,b=1,col=2)
 
 
+gg <- ggplot(data = wgs, mapping = aes(sample = statistic)) +
+  stat_qq_band() +
+  stat_qq_line() +
+  stat_qq_point() +
+  labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
+gg
+
+fp <- file.path(proj_data_dir, 'out', 'qq_plot_sibsum_sibdiff_wgs_only.png')
+ggsave(fp, plot=gg, width = 6, height = 5, units = 'in', dpi = 300)
+
+
+## ---------------------------------------------------------------------
